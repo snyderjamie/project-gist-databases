5a7fd626ed256bc3370b176886eb7778f9539b58
snyderj <jamielsnyder@gmail.com>
2019-09-05 15:41:04 +0000

    Change gists_database/importer.py, gists_database/search.py, & tests/tes_search.py to pass all the test, even the optional.

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,37 @@
 import requests
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    url = 'https://api.github.com/users/{username}/gists'
+    response = requests.get(url.format(username=username))
+    response.raise_for_status()
+    
+    for gist in response.json():
+        gist_db_script = """
+            INSERT INTO gists ('github_id', 'html_url', 'git_pull_url', 
+            'git_push_url', 'commits_url', 'forks_url', 'public', 
+            'created_at', 'updated_at', 'comments', 'comments_url')
+            VALUES (:github_id, :html_url, :git_pull_url, 
+            :git_push_url, :commits_url, :forks_url, :public, 
+            :created_at, :updated_at, :comments, :comments_url
+            )""" 
+        params = {
+            'github_id': gist['id'], 
+            'html_url': gist['html_url'], 
+            'git_pull_url': gist['git_pull_url'], 
+            'git_push_url': gist['git_push_url'], 
+            'commits_url': gist['commits_url'], 
+            'forks_url': gist['forks_url'], 
+            'public': gist['public'], 
+            'created_at': gist['created_at'], 
+            'updated_at': gist['updated_at'], 
+            'comments': gist['comments'], 
+            'comments_url': gist['comments_url']
+            }
+        db.execute(gist_db_script, params)
+        
+    return db
+    
+
+"""
+You are going to use the GitHub gists API to retrieve the gists of a given user, insert those gists into a database (schema may be found in the schema.sql file), and if commit is True, commit those changes to the database.
+"""
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,50 @@
 from .models import Gist
+import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    if kwargs == {}:
+        results = db_connection.execute('SELECT * FROM gists')
+        return results.fetchall()
+    elif kwargs.get('github_id'):
+        results = db_connection.execute('SELECT * FROM gists WHERE github_id= :github_id', kwargs)
+        return [Gist(gist) for gist in results]
+    elif kwargs.get('created_at'):
+        results = db_connection.execute('SELECT * FROM gists WHERE datetime(created_at)= datetime(:created_at)', kwargs)
+        return [Gist(gist) for gist in results]
+    
+    
+    elif '__' in kwargs.keys():
+        key, comparison = kwargs.keys().split('__')
+        comparison = operator_dic[comparison]
+        if key == 'created_at':
+            results = db_connection.execute('SELECT * FROM gists WHERE datetime(created_at) ' + comparison + datetime(kwargs.keys()), kwargs)
+            return [Gist(gist) for gist in results] 
+    
+    #Optional tests (not dynamic)
+    elif kwargs.get('created_at__gt'):
+        results = db_connection.execute('SELECT * FROM gists WHERE datetime(created_at)   > datetime(:created_at__gt)', kwargs)
+        return [Gist(gist) for gist in results]
+    elif kwargs.get('created_at__gte'):
+        results = db_connection.execute('SELECT * FROM gists WHERE datetime(created_at)   >= datetime(:created_at__gte)', kwargs)
+        return [Gist(gist) for gist in results]
+    elif kwargs.get('created_at__lt'):
+        results = db_connection.execute('SELECT * FROM gists WHERE datetime(created_at)   < datetime(:created_at__lt)', kwargs)
+        return [Gist(gist) for gist in results]
+    elif kwargs.get('created_at__lte'):
+        results = db_connection.execute('SELECT * FROM gists WHERE datetime(created_at)   <= datetime(:created_at__lte)', kwargs)
+        return [Gist(gist) for gist in results]
+    
+    
+    elif kwargs.get('updated_at__gt'):
+        results = db_connection.execute('SELECT * FROM gists WHERE datetime(updated_at)   > datetime(:updated_at__gt)', kwargs)
+        return [Gist(gist) for gist in results]
+    elif kwargs.get('updated_at__gte'):
+        results = db_connection.execute('SELECT * FROM gists WHERE datetime(updated_at)   >= datetime(:updated_at__gte)', kwargs)
+        return [Gist(gist) for gist in results]
+    elif kwargs.get('updated_at__lt'):
+        results = db_connection.execute('SELECT * FROM gists WHERE datetime(updated_at)   < datetime(:updated_at__lt)', kwargs)
+        return [Gist(gist) for gist in results]
+    elif kwargs.get('updated_at__lte'):
+        results = db_connection.execute('SELECT * FROM gists WHERE datetime(updated_at)   <= datetime(:updated_at__lte)', kwargs)
+        return [Gist(gist) for gist in results]
+    
